46eadbbfa0a8166dabfcdf527fc804a570827573
Tom Paulus <tom@tompaulus.com>
2017-05-10 14:03:08 -0700

    Added Recorder Sync Job

---
+++ src/main/java/edu/sdsu/its/API/Models/Recorder.java
@@ -0,0 +1,22 @@
+package edu.sdsu.its.API.Models;
+
+import edu.sdsu.its.Mediasite.Recorders;
+
+import java.sql.Timestamp;
+
+/**
+ * @author Tom Paulus
+ *         Created on 5/10/17.
+ */
+public class Recorder extends Recorders.Recorder {
+    Boolean online;
+    Timestamp lastSeen;
+
+    public Recorder(String id, String name, String description, String serialNumber, String version,
+                    String lastVersionUpdateDate, String physicalAddress, String imageVersion,
+                    Boolean online, Timestamp lastSeen) {
+        super(id, name, description, serialNumber, version, lastVersionUpdateDate, physicalAddress, imageVersion);
+        this.online = online;
+        this.lastSeen = lastSeen;
+    }
+}
